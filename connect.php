4609e3ad38d7d6af936d7f76d306fa33581ed259
Tsepo SHATA <tshata@e5r10p1.wethinkcode.co.za>
2018-10-19 14:19:47 +0200

    fixed webcam,need screenshot

--- connect.php
+++ connect.php
@@ -1,18 +1,3 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "Lw7en3xBSylH3KN1";
-
-//create connection
-
-$connection = new mysqli($servername, $username, $password);
-
-//check connection
-
-if ($connection->connect_error)
-{
-  die("Connection failed: " .$connection->connect_error);
-}
-//echo "Connection successful";
 ?>
